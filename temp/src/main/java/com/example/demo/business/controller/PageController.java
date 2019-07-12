e17729b0c1eec7f2c69b8ca5843a2ef9f7b7308b
494195988 <52749136+494195988@users.noreply.github.com>
2019-07-13 00:20:57 +0800

    Update PageController.java

--- temp/src/main/java/com/example/demo/business/controller/PageController.java
+++ temp/src/main/java/com/example/demo/business/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-	@RequestMapping("/web/{page}")
+	@RequestMapping("/web")
 	public Object page() {
 
 		return "index.html";
