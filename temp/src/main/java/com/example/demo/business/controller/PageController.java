06fefe121ca6e165637c4effdbb56d67f13d8e2d
yanglei <958623975@qq.com>
2019-07-14 15:55:34 +0800

    样式回退

--- temp/src/main/java/com/example/demo/business/controller/PageController.java
+++ temp/src/main/java/com/example/demo/business/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-	@RequestMapping("/web")
+	@RequestMapping("/web/{page}")
 	public Object page() {
 
 		return "index.html";
