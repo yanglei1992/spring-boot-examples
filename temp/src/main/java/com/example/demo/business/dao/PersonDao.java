2b8f27b18b2ef006e7310baed6a5cb8fdd4bf870
chenxu <494195988@qq.com>
2019-08-21 15:08:50 +0800

    Update PersonDao.java

--- temp/src/main/java/com/example/demo/business/dao/PersonDao.java
+++ temp/src/main/java/com/example/demo/business/dao/PersonDao.java
@@ -1,10 +1,6 @@
 package com.example.demo.business.dao;
 
-import com.example.demo.business.entity.Person;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
 
 /**
  * 湖北金鹏信息技术有限公司 版权所有 Copyright 2018<br>
@@ -14,8 +10,8 @@ import java.util.List;
  * @CreateDate: Created in 2019/7/10 22:30 <br>
  * @Author: chenx
  */
-@Mapper
+//@Mapper
 public interface PersonDao {
-	@Select("select id,name,age from person")
-	List<Person> getAll();
+	/*@Select("select id,name,age from person")
+	List<Person> getAll();*/
 }
